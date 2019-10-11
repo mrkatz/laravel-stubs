1a86608b3405cc5d7f5a78230e4431ba2fc56da6
Adam <adamkaczocha@gmail.com>
2019-10-11 22:29:39 +1100

    updated travis file

--- tests/Feature/InitialTest.php
+++ tests/Feature/InitialTest.php
@@ -1,14 +1,11 @@
 <?php
 
-
 namespace Mrkatz\LaravelStubs\Tests;
-
 
 use Orchestra\Testbench\TestCase;
 
 class InitialTest extends TestCase
 {
-
     /** @test */
     public function testBasicTest()
     {
