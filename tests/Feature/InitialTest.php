56561c0463e414da801bd205da62a687e7f92a7d
Adam <adamkaczocha@gmail.com>
2019-10-11 22:09:43 +1100

    added tests

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Mrkatz\LaravelStubs\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+
+    /** @test */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
